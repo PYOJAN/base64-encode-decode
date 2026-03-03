0ea1b292e849d5c34fd87a86bb129713315b91a6
Pintu Prajapati <trexolab@outlook.com>
2026-03-03 19:22:54 +0530

    Updates

--- assets/label-CNABkky4.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as r,J as t,a1 as l}from"./index-BrcdSiH6.js";const n=l("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),d=o.forwardRef(({className:a,...e},s)=>r.jsx("label",{ref:s,className:t(n(),a),...e}));d.displayName="Label";export{d as L};
